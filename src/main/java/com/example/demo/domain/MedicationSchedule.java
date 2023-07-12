aa8716ac27b3ba5729e9ab721748160809afc6bd
soyoon1 <20210841@duksung.ac.kr>
2023-07-12 21:01:14 +0900

    RDS MariaDB 연결

--- src/main/java/com/example/demo/domain/MedicationSchedule.java
+++ src/main/java/com/example/demo/domain/MedicationSchedule.java
@@ -29,4 +29,8 @@ public class MedicationSchedule {
     private int dosage;
     private String day_of_week;
     private LocalTime time_of_day;  // 나중에 포맷을 정해줘야 함.
+
+    // 약을 먹은 시간
+    private LocalTime time_of_taking; // 값이 있는지 없는지로 약 복용 판단. String으로 취급해도 괜찮을까
+
 }
