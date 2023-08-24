9c3f40d738e03f7e258a77847c7f8cd6ce2ca512
kbannie <kabin2205@gmail.com>
2023-08-25 03:36:22 +0900

    API에 맞게 작성 / 레포 저장 가능 / 삭제, 수정 기능 추가함/user와의 연결 생각해봐야함

--- src/main/java/com/example/demo/domain/Board.java
+++ src/main/java/com/example/demo/domain/Board.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Setter
 @Getter // get 함수를 일괄적으로 만들어줍니다.
 @NoArgsConstructor // 기본 생성자를 만들어줍니다.
-@Entity(name = "users") // DB 테이블 역할을 합니다.
+@Entity
 public class Board extends Timestamped {
     // 글 고유 아이디
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -26,22 +26,17 @@ public class Board extends Timestamped {
     @Column(nullable = false)
     private String content;
 
-    // 비밀번호
-    @Column(nullable = false)
-    private String password;
 
     // requestDto 정보를 가져와서 entity 만들 때 사용
     public Board(BoardRequestDto requestDto) {
         this.title = requestDto.getTitle();
         this.content = requestDto.getContent();
-        this.password = requestDto.getPassword();
     }
 
     // 업데이트 메소드
     public void update(BoardRequestDto requestDto) {
         this.title = requestDto.getTitle();
         this.content = requestDto.getContent();
-        this.password = requestDto.getPassword();
     }
 
 }
