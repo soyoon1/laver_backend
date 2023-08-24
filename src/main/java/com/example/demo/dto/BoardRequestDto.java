3b90df60ced2d2346aa5240abb97fcf81a5bb846
kbannie <kabin2205@gmail.com>
2023-08-25 03:20:50 +0900

    게시글 올리기 가능 (레포 저장 및 엔티티 수정 전 코드)

---
+++ src/main/java/com/example/demo/dto/BoardRequestDto.java
@@ -0,0 +1,15 @@
+package com.example.demo.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class BoardRequestDto {
+
+    private String title;
+
+    private String content;
+
+    private String password;
+}
