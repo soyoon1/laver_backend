c05f388a8cf8761727a3815cce09aac9ce7f49ca
soyoon1 <20210841@duksung.ac.kr>
2023-09-01 01:18:53 +0900

    amazon S3 연결

--- src/main/java/com/example/demo/controller/MainPageController.java
+++ src/main/java/com/example/demo/controller/MainPageController.java
@@ -2,12 +2,15 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.MainDto;
 import com.example.demo.service.MedicationService;
+import com.example.demo.service.S3UploadService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Slf4j
 @RequestMapping("/api")
@@ -17,10 +20,23 @@ public class MainPageController {
 
     private final MedicationService medicationService;
 
+    private final S3UploadService s3UploadService;
+
     @GetMapping("/main")
     public ResponseEntity<?> getMainPage(){
         MainDto mainDto = medicationService.requestMainPage();
         return ResponseEntity.ok(mainDto);
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file")MultipartFile file){
+        try {
+            String fileUrl = s3UploadService.saveFile(file);
+            return ResponseEntity.ok(fileUrl);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
 }
---
+++ src/main/java/com/example/demo/service/S3UploadService.java
@@ -0,0 +1,56 @@
+package com.example.demo.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class S3UploadService {
+
+    private final AmazonS3 amazonS3;
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Transactional
+    public String saveFile(MultipartFile multipartFile) throws IOException{ // 파일 업로드
+        String originalFilename = multipartFile.getOriginalFilename();
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(multipartFile.getSize());
+        metadata.setContentType(multipartFile.getContentType());
+
+        amazonS3.putObject(bucket, originalFilename, multipartFile.getInputStream(), metadata);
+        return amazonS3.getUrl(bucket, originalFilename).toString();
+
+    }
+
+    @Transactional
+    public ResponseEntity<UrlResource> downloadImage(String originalFilename){ // 파일 다운로드
+        UrlResource urlResource = new UrlResource(amazonS3.getUrl(bucket, originalFilename));
+
+        String contentDisposition = "attachment; filename=\"" + originalFilename + "\"";
+
+        // header에 CONTENT_DISPOSITION 설정을 통해 클릭 시 다운로드 진행
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(urlResource);
+    }
+
+
+
+}
